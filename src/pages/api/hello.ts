d0033f79c42a0868443e07f96f3058f69d115307
Brandon Lok (Qi Luo) <branlok95@gmail.com>
2023-04-05 12:14:20 -0400

    .

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -1,17 +1,9 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
-
-type Data = {
-  name: string
-}
+import type { NextRequest } from 'next/server'
 
 export const config = {
   runtime: 'edge',
 }
 
-export default function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<Data>
-) {
-  res.status(200).json({ name: 'John Doe' })
+export default async function handler(req: NextRequest) {
+  return new Response(JSON.stringify({ name: 'John Doe' }))
 }
