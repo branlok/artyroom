29f78fa6a9e83ed24339d24529305e91ca85a737
Brandon Lok (Qi Luo) <branlok95@gmail.com>
2023-04-11 16:12:44 -0400

    .

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,14 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import { Auth0Provider } from "@auth0/auth0-react";
-console.log(process.env.CF_PAGES);
+
+
 export default function App({ Component, pageProps }: AppProps) {
   return <Auth0Provider
     domain='dev-l5iyg4ax8rlhl2ha.us.auth0.com'
     clientId='3TYRMTQ9odZIKGXMy1ZyKkKNtBm7QgT4'
     authorizationParams={{
-      redirect_uri: process.env.CF_PAGES === "1" ? "https://artyroom.pages.dev" : "http://localhost:3000"
+      redirect_uri: "https://artyroom.pages.dev"
     }}
   >
     <Component {...pageProps} />
