55f9eda6ad3c5af5ee0e97126bb6d763f96fa389
Brandon Lok (Qi Luo) <branlok95@gmail.com>
2023-04-11 15:43:46 -0400

    .

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,15 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { Auth0Provider } from "@auth0/auth0-react";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <Auth0Provider
+    domain='dev-l5iyg4ax8rlhl2ha.us.auth0.com'
+    clientId='3TYRMTQ9odZIKGXMy1ZyKkKNtBm7QgT4'
+    authorizationParams={{
+      redirect_uri: "http://localhost:3000"
+    }}
+  >
+    <Component {...pageProps} />
+  </Auth0Provider>
 }
